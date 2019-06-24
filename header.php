1d7689498212b438901430c422a7d6d71e998fd6
Scott Goodacre <scott@scottgoodacre.co.uk>
2019-06-24 21:12:47 +0100

    Update header.php

--- header.php
+++ header.php
@@ -19,10 +19,24 @@
 	<?php wp_head(); ?>
 	
 	<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+    
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8T99VH');</script>
+<!-- End Google Tag Manager -->
 
 </head>
 
 <body <?php body_class(); ?>>
+    
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8T99VH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
 
